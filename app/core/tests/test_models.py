d246dd29c7005ac51ba81ce9763bbdc0fc9533ec
Cypherlein <114693404+Cypherlein@users.noreply.github.com>
2024-11-26 15:54:25 +0100

    Add new line on file ends

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -5,9 +5,9 @@ Tests for models
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
-
 class ModelTests(TestCase):
     """Test models."""
+
 
     def test_create_user_with_email_successful(self):
         """Test creating a user with an email is successful."""
@@ -17,7 +17,6 @@ class ModelTests(TestCase):
             email=email,
             password=password,
         )
-
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
 
@@ -44,6 +43,5 @@ class ModelTests(TestCase):
             'test@example.com',
             'test123'
         )
-
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
