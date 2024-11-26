d73d17586b807341abc77927f18788bebadeddb6
Cypherlein <114693404+Cypherlein@users.noreply.github.com>
2024-11-26 14:06:13 +0100

    Fix code errors

--- app/core/admin.py
+++ app/core/admin.py
@@ -13,6 +13,7 @@ class UserAdmin(BaseUserAdmin):
     list_display = ['email', 'name']
     fieldsets = (
         (None, {'fields': ('email', 'password')}),
+        (_('Personal Info'), {'fields': ('name',)})
         (
             _('Permissions'),
             {
@@ -22,7 +23,7 @@ class UserAdmin(BaseUserAdmin):
                     'is_superuser',
                 )
             }
-        )
+        ),
         (_('Important dates'), {'fields': ('last_login',)}),
     )
     readonly_fields = ['last_login']
